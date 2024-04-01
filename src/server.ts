4535c018393b4cbe80c46cd79221779d28d955ce
mariopmon <161736021+mariopmon@users.noreply.github.com>
2024-04-01 01:50:45 +0000

    add server config.

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
